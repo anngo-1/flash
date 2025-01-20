b76707063066dd2a4e15f1d2fa28db803b47ede9
anngo-1 <andngo2004@gmail.com>
2025-01-20 07:12:45 -0800

    support for vim input handler, make it default

--- flashcards.py
+++ flashcards.py
@@ -1,19 +1,32 @@
 import curses
+import argparse
 from ui.main import TUI
+from ui.input_handler import SimpleInputHandler
+from ui.vim_input_handler import VimInputHandler
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Flashcard Application")
+    parser.add_argument('--novim', action='store_true', help='Use Vim-style input mode')
+    return parser.parse_args()
 
 def main():
     """Main function to initialize and run the TUI."""
+    args = parse_args()
+    
     try:
         stdscr = curses.initscr()
-        curses.noecho()  
-        curses.cbreak() 
-        stdscr.keypad(True) 
-        app = TUI(stdscr)
+        curses.noecho()
+        curses.cbreak()
+        stdscr.keypad(True)
+
+        InputHandlerClass = SimpleInputHandler if args.novim else VimInputHandler
+        input_handler = InputHandlerClass(stdscr)
+
+        app = TUI(stdscr, input_handler)
         app.run()
-    
+
     except KeyboardInterrupt:
-        pass  
-    
+        pass
     finally:
         if 'stdscr' in locals():
             stdscr.keypad(False)
